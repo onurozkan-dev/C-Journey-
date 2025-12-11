e618bd1e23a0bff61486eafb53c88061041c33a9
Onur Özkan <onurozkn.dev@gmail.com>
2025-12-11 22:34:51 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+
+int main(){
+
+//thats my first coomment in c language!
+
+/* Diferrent way for comment*/
+
+printf("Hello C World!\n");
+    
+printf("I'll be programmer one day!");
+
+
+return 0;
+}
