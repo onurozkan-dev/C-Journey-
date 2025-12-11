fa7059a9cc0131c8ec8ac7e29ac44809379315b9
Onur Özkan <onurozkn.dev@gmail.com>
2025-12-12 00:13:57 +0300

    C Language variables

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 
 
-
+/*My second little project for C language*/
+/* -Onur Özkan 17 y.o */
 
 int main(){
+//You can use int for constants like age
+ int age = 17;
 
-//thats my first coomment in c language!
+ printf("I am %d years old.\n", age);
 
-/* Diferrent way for comment*/
+//GPA, PI had decimal values, we should use float or double buddy!
 
-printf("Hello C World!\n");
-    
-printf("I'll be programmer one day!");
+//Btw if you wanna change the numbers after the dot, use ".nf" where n is the number of digits you want!
+
+ float gpa = 3.5;
+
+ printf("MY GPA is %.1f.\n", gpa);
+
+double pi = 3.141592653589793;
+
+ printf("The value of PI is %.5f.\n", pi);
+
+//char is used for single characters, like grade
+
+char grade = 'A';
+
+printf("My math grade is %c.\n", grade);
+
 
 
 return 0;
